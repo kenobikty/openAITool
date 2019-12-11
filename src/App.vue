2d21d3c9691d72b948d8bc584ef06ab7b97ff555
kenobi <ken.ktyken10@gmail.com>
2019-12-11 13:25:07 +0800

    [deploy.sh] change deploy dist

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="This is Dimir's First github page"/>
   </div>
 </template>
 
